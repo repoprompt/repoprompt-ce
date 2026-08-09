5bf4febea20bc470766996de5bcf9dbdae2acf55
Daniel Sebban <daniel.sebban@gmail.com>
2026-08-09 15:45:25 +0300

    Simplify Linux parent executable lookup

--- Sources/RepoPromptMCP/DirectHeadlessClientIdentity.swift
+++ Sources/RepoPromptMCP/DirectHeadlessClientIdentity.swift
@@ -11,12 +11,9 @@ enum DirectHeadlessClientIdentity {
 #if canImport(Darwin)
         CLIEventLogger.detectClientName()
 #elseif canImport(Glibc)
-        let link = "/proc/\(getppid())/exe"
-        var buffer = [CChar](repeating: 0, count: Int(PATH_MAX) + 1)
-        let count = readlink(link, &buffer, buffer.count - 1)
-        guard count > 0 else { return nil }
-        buffer[Int(count)] = 0
-        return URL(fileURLWithPath: String(cString: buffer)).lastPathComponent
+        rpExecutablePath(processID: getppid()).map {
+            URL(fileURLWithPath: $0).lastPathComponent
+        }
 #else
         nil
 #endif
